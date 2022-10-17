bc7d033a501f2e32bf5ff63c96fb9c1b56bf456c
Abstractiox <106570157+Abstractiox@users.noreply.github.com>
2022-10-17 15:59:42 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 
 function goto12() {
-    location.replace= "https://www.roblox.com";
+    location.replace= ("https://abstractiox.github.io/Abstractiox_Mods/")
 }
 
-function Graphics_Card_OverclockerDownload() {
-    location.replace("/ModWebstite_Abstractiox/index.html")
+function Mod1Download() {
+    location.replace("#")
 
 
 
