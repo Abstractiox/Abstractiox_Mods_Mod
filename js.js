eb71ab38ce66366b282d060ef778d332db2a28d6
Abstractiox <106570157+Abstractiox@users.noreply.github.com>
2022-10-17 14:56:03 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 function goto2() {
-    location.replace("https://abstractiox.github.io/Abstractiox_Mods/index.html")
+    location.href = "https://abstractiox.github.io/Abstractiox_Mods/index.html";
 }
 
 function Graphics_Card_OverclockerDownload() {
